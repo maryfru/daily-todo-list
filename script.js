dcd3128b532b5d9f98d07bcaa5451f945fab3d63
Mary-Ann Johanna Frunzete <maryannfrunzete@gmail.com>
2023-05-25 17:26:38 +0300

    saving todos to local storage

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUl = document.getElementById("todos");
 
+// Basically how lS (Browser API) works:
+// You give it a Key and a string(if it is an OBJ... you need to use JSON.stringify)
+// localStorage.setItem("name", JSON.stringify(obj));
+
+// When you pull it out of lS and it is stringified, then you need to put it in JSON.parse()
+// JSON.parse(localStorage.getItem(obj));
+// LocalStorage
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
+
 // Rules and button event
 rulesBtn.addEventListener("click", () => rules.classList.add("show"));
 closeBtn.addEventListener("click", () => rules.classList.remove("show"));
@@ -24,7 +37,7 @@ function addTodo(todo) {
     todoText = todo.text;
   }
   // showing the value you are typing in the form
-  console.log(todoText);
+  // console.log(todoText);
 
   if (todoText) {
     const todoEl = document.createElement("li");
@@ -35,19 +48,37 @@ function addTodo(todo) {
     todoEl.innerText = todoText;
 
     // will mark the todo as completed
-    todoEl.addEventListener("click", () =>
-      todoEl.classList.toggle("completed")
-    );
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed"), updateLS();
+    });
 
     // will delete the todo
     todoEl.addEventListener("contextmenu", (e) => {
       e.preventDefault();
 
       todoEl.remove();
+      updateLS();
     });
 
     todosUl.appendChild(todoEl);
 
     input.value = "";
+
+    updateLS();
   }
+}
+
+function updateLS() {
+  todosEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
